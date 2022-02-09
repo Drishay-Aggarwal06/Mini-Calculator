22bfbd415904b890df4521797558bbb653c0d98d
Drishay-Aggarwal06 <99349537+Drishay-Aggarwal06@users.noreply.github.com>
2022-02-09 21:53:36 +0530

    Add files via upload

---
+++ # minicalculater .py
@@ -0,0 +1,16 @@
+# Mini Calculater Made By Drishay Aggarwal
+a=int(input("Enter the first number:"))
+o=input("Entetr operator: +,-,*,/,%:")
+b=int(input("Enter the second number:"))
+if o=="+":
+    print(a+b)
+elif o=="-":
+    print(a-b)
+elif o=="*":
+    print(a*b)
+elif o=="/":
+    print(a/b)
+elif o=="%":
+    print(a%b)
+else:
+    print("Go and learn maths first")
